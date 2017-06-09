35ea47e996c570bf4a033e2324aef1ffd344c0c7
Antti Kangasrääsiö <antti.kangasraasio@hiit.fi>
2017-06-09 12:36:37 +0300

    Add 2d mesh evaluation utility

--- elfie/utils.py
+++ elfie/utils.py
@@ -1,4 +1,5 @@
 import json
+import numpy as np
 
 import logging
 logger = logging.getLogger(__name__)
@@ -18,3 +19,26 @@ def read_json_file(filename):
     logger.info("Read {}".format(filename))
     return data
 
+def eval_2d_mesh(xmin, ymin, xmax, ymax, nx, ny, eval_fun):
+    """
+        Evaluate 'eval_fun' at a grid defined by max and min
+        values with number of points defined by 'nx' and 'ny'.
+    """
+    if xmin > xmax:
+        raise ValueError("xmin (%.2f) was greater than"
+                         "xmax (%.2f)" % (xmin, xmax))
+    if ymin > ymax:
+        raise ValueError("ymin (%.2f) was greater than"
+                         "ymax (%.2f)" % (xmin, xmax))
+    if nx < 1 or ny < 1:
+        raise ValueError("nx (%.2f) or ny (%.2f) was less than 1" % (nx, ny))
+    X = np.linspace(xmin, xmax, nx)
+    lenx = len(X)
+    Y = np.linspace(ymin, ymax, ny)
+    leny = len(Y)
+    X, Y = np.meshgrid(X, Y)
+    Z = np.zeros((leny, lenx))
+    for i in range(leny):
+        for j in range(lenx):
+            Z[i][j] = eval_fun([X[i][j], Y[i][j]])
+    return (X, Y, Z)
