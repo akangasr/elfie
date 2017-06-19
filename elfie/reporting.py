e6bafd87c867f39b6c54f492e0a4b3b44c58ba16
Antti Kangasrääsiö <antti.kangasraasio@hiit.fi>
2017-06-19 10:43:53 +0300

    Rename default result json file

--- elfie/reporting.py
+++ elfie/reporting.py
@@ -7,7 +7,7 @@ from elfie.utils import write_json_file
 def run_and_report(experiment,
                    path,
                    pdf_file="results.pdf",
-                   log_file="experiment.json",
+                   log_file="results.json",
                    figsize=(8.27, 11.69)):
     pdf_file = os.path.join(path, pdf_file)
     log_file = os.path.join(path, log_file)
